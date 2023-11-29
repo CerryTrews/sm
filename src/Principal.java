81c121fce6b082445795322968bb130de9562615
Cerry <denis.carreto@hotmail.com>
2023-11-29 13:05:54 -0300

    Atualização ScreenMatch

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,12 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoMinutos = 189;
-        meuFilme.inclusoNoPlano = true;
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoMinutos(189);
+        meuFilme.setInclusoNoPlano(true);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,59 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean inclusoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalAvaliacoes;
+    private int duracaoMinutos;
+
+    public int getTotalAvaliacoes(){ return totalAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setInclusoNoPlano(boolean inclusoNoPlano) {
+        this.inclusoNoPlano = inclusoNoPlano;
+    }
+
+    public void setDuracaoMinutos(int duracaoMinutos) {
+        this.duracaoMinutos = duracaoMinutos;
+    }
+
+    public void exibeFichaTecnica () {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isInclusoNoPlano() {
+        return inclusoNoPlano;
+    }
+
+    public int getDuracaoMinutos() {
+        return duracaoMinutos;
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalAvaliacoes++;
+    }
+
+    public double pegaMedia() {
+        return somaDasAvaliacoes / totalAvaliacoes;
+    }
+}
